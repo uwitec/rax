091c02b0a18db6da45983b7f93f27b0d3086eb58
Donyad <Donyad@37f794f0-8139-0410-a0ea-f94afe7f1094>
2008-03-27 03:31:01 +0000

    实现部分ArticleCategoryDao

--- trunk/Rax/src/rax/dao/ArticleCategoryDao.java
+++ trunk/Rax/src/rax/dao/ArticleCategoryDao.java
@@ -8,36 +8,39 @@ import rax.model.ArticleCategory;
 
 public interface ArticleCategoryDao {
 
-    public int create(ArticleCategory newInstance) throws DataAccessException;
+	public int create(ArticleCategory newInstance) throws DataAccessException;
 
-    public ArticleCategory read(int id) throws DataAccessException;
+	public ArticleCategory read(int id) throws DataAccessException;
 
-    public int update(ArticleCategory transientObject)
-            throws DataAccessException;
+	public int update(ArticleCategory transientObject)
+			throws DataAccessException;
 
-    public int delete(ArticleCategory persistentObject)
-            throws DataAccessException;
+	public int delete(ArticleCategory persistentObject)
+			throws DataAccessException;
 
-    public int countByCategoryId(int id) throws DataAccessException;
+	public int countByCategoryId(int id) throws DataAccessException;
 
-    public List<ArticleCategory> listAll() throws DataAccessException;
+	public int countAllSubCategoryByCategoryId(int id)
+			throws DataAccessException;
 
-    public List<ArticleCategory> listByCategoryId(int id, int index, int num)
-            throws DataAccessException;
+	public List<ArticleCategory> listAll() throws DataAccessException;
 
-    public List<ArticleCategory> listAllByCategoryId(int id)
-            throws DataAccessException;
+	public List<ArticleCategory> listByCategoryId(int id, int index, int num)
+			throws DataAccessException;
 
-    public List<ArticleCategory> listSubCategoryByCategoryId(int id, int index,
-            int num) throws DataAccessException;
+	public List<ArticleCategory> listAllByCategoryId(int id)
+			throws DataAccessException;
 
-    public List<ArticleCategory> listAllSubCategoryByCategoryId(int id)
-            throws DataAccessException;
+	public List<ArticleCategory> listSubCategoryByCategoryId(int id, int index,
+			int num) throws DataAccessException;
 
-    public int adjustLthread(int from, int diff) throws DataAccessException;
+	public List<ArticleCategory> listAllSubCategoryByCategoryId(int id)
+			throws DataAccessException;
 
-    public int adjustRthread(int from, int diff) throws DataAccessException;
+	public int adjustLthread(int from, int diff) throws DataAccessException;
 
-    public List<ArticleCategory> listPathToCategory(int lthread, int rthread)
-            throws DataAccessException;
+	public int adjustRthread(int from, int diff) throws DataAccessException;
+
+	public List<ArticleCategory> listPathToCategory(int lthread, int rthread)
+			throws DataAccessException;
 }
--- trunk/Rax/src/rax/dao/ibatis/ArticleCategoryDaoImpl.java
+++ trunk/Rax/src/rax/dao/ibatis/ArticleCategoryDaoImpl.java
@@ -11,106 +11,123 @@ import rax.dao.ArticleCategoryDao;
 import rax.model.ArticleCategory;
 
 public class ArticleCategoryDaoImpl extends SqlMapClientDaoSupport implements
-        ArticleCategoryDao {
+		ArticleCategoryDao {
 
-    @Override
-    public int create(ArticleCategory newInstance) throws DataAccessException {
-        return (Integer) getSqlMapClientTemplate().insert(
-                "createArticleCategory", newInstance);
-    }
+	@Override
+	public int create(ArticleCategory newInstance) throws DataAccessException {
+		return (Integer) getSqlMapClientTemplate().insert(
+				"createArticleCategory", newInstance);
+	}
 
-    @Override
-    public ArticleCategory read(int id) throws DataAccessException {
-        return (ArticleCategory) getSqlMapClientTemplate().queryForObject(
-                "readArticleCategory", id);
-    }
+	@Override
+	public ArticleCategory read(int id) throws DataAccessException {
+		return (ArticleCategory) getSqlMapClientTemplate().queryForObject(
+				"readArticleCategory", id);
+	}
 
-    @Override
-    public int update(ArticleCategory transientObject)
-            throws DataAccessException {
-        return getSqlMapClientTemplate().update("updateArticleCategory",
-                transientObject);
-    }
+	@Override
+	public int update(ArticleCategory transientObject)
+			throws DataAccessException {
+		return getSqlMapClientTemplate().update("updateArticleCategory",
+				transientObject);
+	}
 
-    @Override
-    public int delete(ArticleCategory persistentObject)
-            throws DataAccessException {
-        return getSqlMapClientTemplate().delete("deleteArticleCategory",
-                persistentObject.getId());
-    }
+	@Override
+	public int delete(ArticleCategory persistentObject)
+			throws DataAccessException {
+		return getSqlMapClientTemplate().delete("deleteArticleCategory",
+				persistentObject.getId());
+	}
 
-    @Override
-    public int countByCategoryId(int id) throws DataAccessException {
-        return (Integer) getSqlMapClientTemplate().queryForObject(
-                "countArticleCategoryByCategoryId", id);
-    }
+	@Override
+	public int countByCategoryId(int id) throws DataAccessException {
+		return (Integer) getSqlMapClientTemplate().queryForObject(
+				"countDirectArticleCategoryByCategoryId", id);
+	}
 
-    @Override
-    public List<ArticleCategory> listAll() throws DataAccessException {
-        return getSqlMapClientTemplate().queryForList("listAllArticleCategory");
-    }
+	@Override
+	public int countAllSubCategoryByCategoryId(int id)
+			throws DataAccessException {
+		ArticleCategory category = read(id);
+		Map param = new HashMap();
+		param.put("lthread", category.getLthread());
+		param.put("rthread", category.getRthread());
+		return (Integer) getSqlMapClientTemplate().queryForObject(
+				"countAllArticleCategoryByCategoryId", param);
+	}
 
-    @Override
-    public List<ArticleCategory> listByCategoryId(int id, int index, int num)
-            throws DataAccessException {
-        Map param = new HashMap();
-        param.put("index", index);
-        param.put("number", num);
-        param.put("categoryId", id);
-        return getSqlMapClientTemplate().queryForList(
-                "listArticleCategoryByCategoryId", param);
-    }
+	@Override
+	public List<ArticleCategory> listAll() throws DataAccessException {
+		return getSqlMapClientTemplate().queryForList("listAllArticleCategory");
+	}
 
-    @Override
-    public List<ArticleCategory> listAllByCategoryId(int id)
-            throws DataAccessException {
-        return getSqlMapClientTemplate().queryForList(
-                "listArticleCategoryByCategoryId", id);
-    }
+	@Override
+	public List<ArticleCategory> listByCategoryId(int id, int index, int num)
+			throws DataAccessException {
+		Map param = new HashMap();
+		param.put("index", index);
+		param.put("number", num);
+		param.put("categoryId", id);
+		return getSqlMapClientTemplate().queryForList(
+				"listDirectArticleCategoryByCategoryId", param);
+	}
 
-    @Override
-    public List<ArticleCategory> listSubCategoryByCategoryId(int id, int index,
-            int num) throws DataAccessException {
-        Map param = new HashMap();
-        param.put("index", index);
-        param.put("number", num);
-        param.put("categoryId", id);
-        return getSqlMapClientTemplate().queryForList(
-                "listSubArticleCategoryByCategoryId", param);
-    }
+	@Override
+	public List<ArticleCategory> listAllByCategoryId(int id)
+			throws DataAccessException {
+		return getSqlMapClientTemplate().queryForList(
+				"listAllDirectArticleCategoryByCategoryId", id);
+	}
 
-    @Override
-    public List<ArticleCategory> listAllSubCategoryByCategoryId(int id)
-            throws DataAccessException {
-        return getSqlMapClientTemplate().queryForList(
-                "listAllSubArticleCategoryByCategoryId", id);
-    }
+	@Override
+	public List<ArticleCategory> listSubCategoryByCategoryId(int id, int index,
+			int num) throws DataAccessException {
+		ArticleCategory category = read(id);
+		Map param = new HashMap();
+		param.put("index", index);
+		param.put("number", num);
+		param.put("lthread", category.getLthread());
+		param.put("rthread", category.getRthread());		
+		return getSqlMapClientTemplate().queryForList(
+				"listSubArticleCategoryByCategoryId", param);
+	}
 
-    @Override
-    public int adjustLthread(int from, int diff) throws DataAccessException {
-        Map param = new HashMap();
-        param.put("from", from);
-        param.put("diff", diff);
-        return getSqlMapClientTemplate().update(
-                "adjustArticleCategoryLeftThread", param);
-    }
+	@Override
+	public List<ArticleCategory> listAllSubCategoryByCategoryId(int id)
+			throws DataAccessException {
+		ArticleCategory category = read(id);
+		Map param = new HashMap();
+		param.put("lthread", category.getLthread());
+		param.put("rthread", category.getRthread());		
+		return getSqlMapClientTemplate().queryForList(
+				"listAllSubArticleCategoryByCategoryId", param);
+	}
 
-    @Override
-    public int adjustRthread(int from, int diff) throws DataAccessException {
-        Map param = new HashMap();
-        param.put("from", from);
-        param.put("diff", diff);
-        return getSqlMapClientTemplate().update(
-                "adjustArticleCategoryRightThread", param);
-    }
+	@Override
+	public int adjustLthread(int from, int diff) throws DataAccessException {
+		Map param = new HashMap();
+		param.put("from", from);
+		param.put("diff", diff);
+		return getSqlMapClientTemplate().update(
+				"adjustArticleCategoryLeftThread", param);
+	}
 
-    @Override
-    public List<ArticleCategory> listPathToCategory(int lthread, int rthread)
-            throws DataAccessException {
-        Map param = new HashMap();
-        param.put("lThread", lthread);
-        param.put("rThread", rthread);
-        return getSqlMapClientTemplate().queryForList("listPathToCategory",
-                param);
-    }
+	@Override
+	public int adjustRthread(int from, int diff) throws DataAccessException {
+		Map param = new HashMap();
+		param.put("from", from);
+		param.put("diff", diff);
+		return getSqlMapClientTemplate().update(
+				"adjustArticleCategoryRightThread", param);
+	}
+
+	@Override
+	public List<ArticleCategory> listPathToCategory(int lthread, int rthread)
+			throws DataAccessException {
+		Map param = new HashMap();
+		param.put("lThread", lthread);
+		param.put("rThread", rthread);
+		return getSqlMapClientTemplate().queryForList("listPathToCategory",
+				param);
+	}
 }
