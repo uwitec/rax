f225c30c2ef4d293f01fb2b8be5e88f35026914c
Donyad <Donyad@37f794f0-8139-0410-a0ea-f94afe7f1094>
2008-05-04 12:53:21 +0000

    调通iText中文定位和区域显示 更新iText库版本

--- trunk/Erp/src/erp/action/ExpressAction.java
+++ trunk/Erp/src/erp/action/ExpressAction.java
@@ -9,8 +9,8 @@ import java.util.Map;
 import org.apache.log4j.Logger;
 
 import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
 import com.lowagie.text.Element;
+import com.lowagie.text.Font;
 import com.lowagie.text.Phrase;
 import com.lowagie.text.Rectangle;
 import com.lowagie.text.pdf.BaseFont;
@@ -45,46 +45,42 @@ public class ExpressAction implements Action {
                 "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
         BaseFont bfEnglish = BaseFont.createFont(BaseFont.HELVETICA,
                 BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+        Font ef = new Font(bfEnglish, 12, Font.NORMAL);
+        Font cf = new Font(bfChinese, 12, Font.NORMAL);
 
-        try {
-            PdfWriter writer = PdfWriter.getInstance(doc, buf);
-            doc.open();
+        PdfWriter writer = PdfWriter.getInstance(doc, buf);
+        doc.open();
 
-            PdfContentByte cb = writer.getDirectContent();
-            cb.beginText();
-            cb.setFontAndSize(bfChinese, 12);
-            cb.setTextMatrix(10, 10);
-            cb.showText("中文内容");
-            
-            ColumnText ct = new ColumnText(cb);
-            ct.setSimpleColumn(10, 100, 100, 200, 15, Element.ALIGN_LEFT | Element.ALIGN_TOP);
-            ct.addText(new Phrase("很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长的中文内容"));
-            ct.go();
-            
-            cb.setTextMatrix(10, 80);
-            cb.showText("很长很长\n很长很长\n很长的\n中文内容");
-            cb.setTextMatrix(10, 120);
-            cb.showText("很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长的中文内容");
+        PdfContentByte cb = writer.getDirectContent();
+        cb.beginText();
+        cb.setFontAndSize(bfChinese, 12);
+        cb.setTextMatrix(10, 10);
+        cb.showText("中文内容");
+        cb.setTextMatrix(10, 80);
+        cb.showText("很长很长\n很长很长\n很长的\n中文内容");
+        cb.setTextMatrix(10, 120);
+        cb.showText("很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长的中文内容");
 
-            cb.setFontAndSize(bfEnglish, 12);
-            cb.setTextMatrix(47, 34);
-            cb.showText(date);
-            cb.endText();
+        cb.setFontAndSize(bfEnglish, 12);
+        cb.setTextMatrix(47, 34);
+        cb.showText(date);
+        cb.endText();
+        
+        Phrase p1 = new Phrase(
+                "quick brown fox jumps over the lazy dog quick brown fox jumps over the lazy dog quick brown fox jumps over the lazy dog",
+                ef);
+        Phrase p2 = new Phrase("很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长的中文", cf);
 
-            /*
-             * doc.add(new Paragraph(date)); doc.add(new Paragraph(comment));
-             * 
-             * doc.add(new Paragraph("HelloWorld")); doc.add(new
-             * Paragraph("中文内容", cf)); doc.add(new Paragraph(new
-             * Date().toString()));
-             */
-
-        } catch (DocumentException ex) {
-            logger.error(ex.getMessage());
-        } finally {
-            doc.close();
-        }
-
+        ColumnText ct = new ColumnText(cb);
+        ct.setSimpleColumn(p1, 100, 100, 200, 200, 15, Element.ALIGN_LEFT
+                | Element.ALIGN_TOP);
+        ct.go();
+        ct.clearChunks();
+        ct.setSimpleColumn(p2, 200, 200, 300, 300, 15, Element.ALIGN_LEFT
+                | Element.ALIGN_TOP);
+        ct.go();
+        
+        doc.close();
         return new ByteArrayInputStream(buf.toByteArray());
     }
 
