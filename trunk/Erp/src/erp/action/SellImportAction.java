375b6fb2655ab7e81392309f44503f4d635f3327
Donyad <Donyad@37f794f0-8139-0410-a0ea-f94afe7f1094>
2009-02-15 04:22:22 +0000

    支持淘宝0215改版，新的地址格式

--- trunk/Erp/src/erp/action/SellImportAction.java
+++ trunk/Erp/src/erp/action/SellImportAction.java
@@ -19,225 +19,229 @@ import erp.service.SellService;
 
 public class SellImportAction extends ActionSupport {
 
-    private static final long serialVersionUID = 1L;
-    private final static Logger logger = Logger
-            .getLogger(SellImportAction.class);
+	private static final long serialVersionUID = 1L;
+	private final static Logger logger = Logger
+			.getLogger(SellImportAction.class);
 
-    private SellService sellService = null;
-    private ExpressService expressService = null;
+	private SellService sellService = null;
+	private ExpressService expressService = null;
 
-    private int sellId;
-    private String content;
-    private String fee;
-    private String feeReal;
-    private int expressId;
-    private String commentExpress;
-    private String commentInvoice;
-    private String sender;
-    private String date;
+	private int sellId;
+	private String content;
+	private String fee;
+	private String feeReal;
+	private int expressId;
+	private String commentExpress;
+	private String commentInvoice;
+	private String sender;
+	private String date;
 
-    List<String> dateSel;
-    Map<Integer, String> expressSel;
+	List<String> dateSel;
+	Map<Integer, String> expressSel;
 
-    public String input() throws Exception {
-        try {
-            Date d = new Date();
-            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-            Calendar c = Calendar.getInstance();
-            c.setTime(d);
+	public String input() throws Exception {
+		try {
+			Date d = new Date();
+			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+			Calendar c = Calendar.getInstance();
+			c.setTime(d);
 
-            if (c.get(Calendar.HOUR_OF_DAY) > 18) {
-                c.setTimeInMillis(d.getTime() + 86400000);
-                d = c.getTime();
-            }
-            date = df.format(d);
+			if (c.get(Calendar.HOUR_OF_DAY) > 18) {
+				c.setTimeInMillis(d.getTime() + 86400000);
+				d = c.getTime();
+			}
+			date = df.format(d);
 
-            Date d1 = new Date();
-            c.setTimeInMillis(d1.getTime() + 86400000);
-            Date d2 = c.getTime();
-            c.setTimeInMillis(d2.getTime() + 86400000);
-            Date d3 = c.getTime();
+			Date d1 = new Date();
+			c.setTimeInMillis(d1.getTime() + 86400000);
+			Date d2 = c.getTime();
+			c.setTimeInMillis(d2.getTime() + 86400000);
+			Date d3 = c.getTime();
 
-            dateSel = new ArrayList<String>();
-            dateSel.add(df.format(d1));
-            dateSel.add(df.format(d2));
-            dateSel.add(df.format(d3));
+			dateSel = new ArrayList<String>();
+			dateSel.add(df.format(d1));
+			dateSel.add(df.format(d2));
+			dateSel.add(df.format(d3));
 
-            fee = "5";
-            feeReal = "4";
-            expressId = 0;
-            expressSel = expressService.getExpressSel();
-        } catch (Exception ex) {
-            logger.error(ex.toString());
-            return ERROR;
-        }
-        return SUCCESS;
-    }
+			fee = "5";
+			feeReal = "4";
+			expressId = 0;
+			expressSel = expressService.getExpressSel();
+		} catch (Exception ex) {
+			logger.error(ex.toString());
+			return ERROR;
+		}
+		return SUCCESS;
+	}
 
-    @Override
-    public String execute() throws Exception {
-        try {
-            Sell obj = (sellId > 0) ? sellService.getSellById(sellId)
-                    : new Sell();
+	@Override
+	public String execute() throws Exception {
+		try {
+			Sell obj = (sellId > 0) ? sellService.getSellById(sellId)
+					: new Sell();
 
-            String[] info = content.split("，");
-            for (int i = 0; i < info.length; i++) {
-                logger.info("info[" + i + "]:" + info[i]);
-            }
+			String[] info = content.split("\n");
+			for (int i = 0; i < info.length; i++) {
+				logger.info("info[" + i + "]:" + info[i]);
+			}
 
-            obj.setCustomerName(info[0].trim());
-            obj.setCustomerPhone1(info[1].trim());
-            obj.setCustomerPhone2(info[2].trim());
-            obj.setCustomerAddress(info[3].trim());
-            obj.setCustomerPostCode(info[4].trim());
-            obj.setExpressId(expressId);
-            obj.setCommentExpress(commentExpress);
-            obj.setCommentInvoice(commentInvoice);
-            obj.setSender(sender);
+			obj.setCustomerName(info[0].substring(info[0].indexOf("：") + 1).trim());
+			obj.setCustomerAddress(info[1].substring(info[1].indexOf("：") + 1).trim());
+			obj.setCustomerPostCode(info[2].substring(info[2].indexOf("：") + 1).trim());
+			String[] phoneList = info[3].substring(info[3].indexOf("：") + 1).trim()
+					.split(" ", 2);
+			
+			obj.setCustomerPhone1(phoneList[0].trim());
+			obj.setCustomerPhone2(phoneList.length > 1 ? phoneList[1].trim() : "");
 
-            if (fee != null && fee.isEmpty() == false) {
-                obj.setFee(Double.parseDouble(fee));
-            }
-            if (feeReal != null && feeReal.isEmpty() == false) {
-                obj.setFeeReal(Double.parseDouble(feeReal));
-            }
+			obj.setExpressId(expressId);
+			obj.setCommentExpress(commentExpress);
+			obj.setCommentInvoice(commentInvoice);
+			obj.setSender(sender);
 
-            if (date != null) {
-                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-                obj.setSendDate(df.parse(date));
-            }
+			if (fee != null && fee.isEmpty() == false) {
+				obj.setFee(Double.parseDouble(fee));
+			}
+			if (feeReal != null && feeReal.isEmpty() == false) {
+				obj.setFeeReal(Double.parseDouble(feeReal));
+			}
 
-            logger.info("Name:" + obj.getCustomerName());
-            logger.info("Phone1:" + obj.getCustomerPhone1());
-            logger.info("Phone2:" + obj.getCustomerPhone2());
-            logger.info("Address:" + obj.getCustomerAddress());
-            logger.info("PostCode:" + obj.getCustomerPostCode());
+			if (date != null) {
+				SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+				obj.setSendDate(df.parse(date));
+			}
 
-            if (sellId > 0) {
-                sellService.updateSell(obj);
-            } else if (sellId == 0) {
-                sellId = sellService.createSell(obj);
-            }
-        } catch (Exception ex) {
-            logger.error(ex.toString());
-            return ERROR;
-        }
-        return SUCCESS;
-    }
+			logger.info("Name:" + obj.getCustomerName());
+			logger.info("Phone1:" + obj.getCustomerPhone1());
+			logger.info("Phone2:" + obj.getCustomerPhone2());
+			logger.info("Address:" + obj.getCustomerAddress());
+			logger.info("PostCode:" + obj.getCustomerPostCode());
 
-    public static void main(String[] args) throws Exception {
-        char[] cbuf = new char[1024];
-        StringBuffer buf = new StringBuffer();
-        InputStreamReader is = new InputStreamReader(new FileInputStream(
-                "D:/address.txt"));
-        int size;
-        while ((size = is.read(cbuf)) != -1) {
-            buf.append(cbuf, 0, size);
-        }
+			if (sellId > 0) {
+				sellService.updateSell(obj);
+			} else if (sellId == 0) {
+				sellId = sellService.createSell(obj);
+			}
+		} catch (Exception ex) {
+			logger.error(ex.toString());
+			return ERROR;
+		}
+		return SUCCESS;
+	}
 
-        SellImportAction action = new SellImportAction();
-        action.content = buf.toString();
-        action.sellId = -1;
-        action.execute();
-    }
+	public static void main(String[] args) throws Exception {
+		char[] cbuf = new char[1024];
+		StringBuffer buf = new StringBuffer();
+		InputStreamReader is = new InputStreamReader(new FileInputStream(
+				"D:/address.txt"));
+		int size;
+		while ((size = is.read(cbuf)) != -1) {
+			buf.append(cbuf, 0, size);
+		}
 
-    public SellService getSellService() {
-        return sellService;
-    }
+		SellImportAction action = new SellImportAction();
+		action.content = buf.toString();
+		action.sellId = -1;
+		action.execute();
+	}
 
-    public void setSellService(SellService sellService) {
-        this.sellService = sellService;
-    }
+	public SellService getSellService() {
+		return sellService;
+	}
 
-    public int getSellId() {
-        return sellId;
-    }
+	public void setSellService(SellService sellService) {
+		this.sellService = sellService;
+	}
 
-    public void setSellId(int sellId) {
-        this.sellId = sellId;
-    }
+	public int getSellId() {
+		return sellId;
+	}
 
-    public String getContent() {
-        return content;
-    }
+	public void setSellId(int sellId) {
+		this.sellId = sellId;
+	}
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+	public String getContent() {
+		return content;
+	}
 
-    public String getFee() {
-        return fee;
-    }
+	public void setContent(String content) {
+		this.content = content;
+	}
 
-    public void setFee(String fee) {
-        this.fee = fee;
-    }
+	public String getFee() {
+		return fee;
+	}
 
-    public String getFeeReal() {
-        return feeReal;
-    }
+	public void setFee(String fee) {
+		this.fee = fee;
+	}
 
-    public void setFeeReal(String feeReal) {
-        this.feeReal = feeReal;
-    }
+	public String getFeeReal() {
+		return feeReal;
+	}
 
-    public int getExpressId() {
-        return expressId;
-    }
+	public void setFeeReal(String feeReal) {
+		this.feeReal = feeReal;
+	}
 
-    public void setExpressId(int expressId) {
-        this.expressId = expressId;
-    }
+	public int getExpressId() {
+		return expressId;
+	}
 
-    public String getSender() {
-        return sender;
-    }
+	public void setExpressId(int expressId) {
+		this.expressId = expressId;
+	}
 
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
+	public String getSender() {
+		return sender;
+	}
 
-    public Map<Integer, String> getExpressSel() {
-        return expressSel;
-    }
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
 
-    public ExpressService getExpressService() {
-        return expressService;
-    }
+	public Map<Integer, String> getExpressSel() {
+		return expressSel;
+	}
 
-    public void setExpressService(ExpressService expressService) {
-        this.expressService = expressService;
-    }
+	public ExpressService getExpressService() {
+		return expressService;
+	}
 
-    public String getCommentExpress() {
-        return commentExpress;
-    }
+	public void setExpressService(ExpressService expressService) {
+		this.expressService = expressService;
+	}
 
-    public void setCommentExpress(String commentExpress) {
-        this.commentExpress = commentExpress;
-    }
+	public String getCommentExpress() {
+		return commentExpress;
+	}
 
-    public String getCommentInvoice() {
-        return commentInvoice;
-    }
+	public void setCommentExpress(String commentExpress) {
+		this.commentExpress = commentExpress;
+	}
 
-    public void setCommentInvoice(String commentInvoice) {
-        this.commentInvoice = commentInvoice;
-    }
+	public String getCommentInvoice() {
+		return commentInvoice;
+	}
 
-    public String getDate() {
-        return date;
-    }
+	public void setCommentInvoice(String commentInvoice) {
+		this.commentInvoice = commentInvoice;
+	}
 
-    public void setDate(String date) {
-        this.date = date;
-    }
+	public String getDate() {
+		return date;
+	}
 
-    public List<String> getDateSel() {
-        return dateSel;
-    }
+	public void setDate(String date) {
+		this.date = date;
+	}
 
-    public void setDateSel(List<String> dateSel) {
-        this.dateSel = dateSel;
-    }
+	public List<String> getDateSel() {
+		return dateSel;
+	}
+
+	public void setDateSel(List<String> dateSel) {
+		this.dateSel = dateSel;
+	}
 }
